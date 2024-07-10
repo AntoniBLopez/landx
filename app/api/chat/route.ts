6e6db672626849e4017702eca599f71f95821881
Jorge <jorgitobotero.os@gmail.com>
2024-07-10 15:20:16 -0500

    feat: Agregue soporte para Groq API

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,21 @@
-import { openai } from "@ai-sdk/openai";
+import { openai, createOpenAI } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+// Modelo gratuito
+const groq = createOpenAI({
+  baseURL: 'https://api.groq.com/openai/v1',
+  apiKey: process.env.GROQ_API_KEY,
+});
+
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: openai("gpt-4-turbo"),
+    // model: openai("gpt-4-turbo"),
+    model: groq('llama3-8b-8192'),
     messages,
   });
 
