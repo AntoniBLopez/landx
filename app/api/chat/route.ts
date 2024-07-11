0e0b52fbb31aba90d96b38adb9b85d9ad7d4f338
JR Full Stack <jrfullstack@gmail.com>
2024-07-11 22:03:28 +0200

    style(shadcn): shadcn installation and configuration

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,24 +1,28 @@
-import { openai, createOpenAI } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { createOpenAI } from '@ai-sdk/openai'
+import { streamText } from 'ai'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
 
 // Modelo gratuito
 const groq = createOpenAI({
-  baseURL: 'https://api.groq.com/openai/v1',
-  apiKey: process.env.GROQ_API_KEY,
-});
+	baseURL: 'https://api.groq.com/openai/v1',
+	apiKey: process.env.GROQ_API_KEY,
+})
 
 export async function POST(req: Request) {
 	const { messages } = await req.json()
 
-  const result = await streamText({
-    /* Usar groq, dejamos la API KEY de openai para usarla cuando despleguemos el proyecto */
-    // model: openai("gpt-4-turbo"),
-    model: groq('llama3-8b-8192'),
-    messages,
-  });
+	// const result = await generateText({
+	const result = await streamText({
+		// model: openai("gpt-4-turbo"),
+		model: groq('llama3-8b-8192'),
+		// system: "Como queremos que se comporte el modelo",
+		messages,
+	})
+
+	// console.log(result.text);
+	// return Response.json(result.text);
 
 	return result.toAIStreamResponse()
 }
