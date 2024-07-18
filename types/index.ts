736d7af0e32bce8fece835c313650f4e732ca093
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-07-19 01:03:51 +0200

    Conflict fixed

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,5 @@
 /* Typescript types */
 
-// Example
 export type ApiKey = string;
 
 export type Theme = "light" | "dark" | "system";
@@ -9,3 +8,15 @@ export type ImageLoaderProps = {
 	src: string;
 	width: number;
 };
+
+export interface FormData {
+	business__name: string;
+	business__description: string;
+	business__email: string;
+	business__CTA: string;
+	style__primaryColor: string;
+	style__secondaryColor: string;
+	style__font: string;
+	style__styleDesingn: string;
+	tech__stack: string;
+}
