fc2840b0af9c913e40d1da07308de7f22030aedd
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-07-20 01:07:29 +0200

    Prompt required

--- components/ui/input-vanisher.tsx
+++ components/ui/input-vanisher.tsx
@@ -225,6 +225,7 @@ export function PlaceholdersAndVanishInput({
 					"w-full relative text-sm sm:text-base z-50 border-none dark:text-white bg-transparent text-black h-full rounded-full focus:outline-none focus:ring-0 pl-4 sm:pl-10 pr-20",
 					animating && "text-transparent dark:text-transparent"
 				)}
+				required
 			/>
 
 			<button
