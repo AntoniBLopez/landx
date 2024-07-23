e1213146c7d64e3e30d8be6dbe21eac5357b840e
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-07-23 02:45:44 +0200

    dashboard: UX/UI improvements

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -3,14 +3,16 @@ import UserLandingCard from '@/app/(main)/dashboard/widgets/UserLandingCard'
 
 export default function Page() {
   return (
-    <main className='mt-28 mx-8'>
+    <main className='mt-28 mb-20 mx-8'>
       <h1 className='text-center py-10'>See all landing pages created by Landex users!</h1>
 
-      <div className='flex flex-col w-full items-center gap-2'>
-        <UserLandingCard />
-        <UserLandingCard />
-        <UserLandingCard />
-        <UserLandingCard />
+      <div className='flex flex-wrap w-full items-center justify-center gap-10'>
+        <UserLandingCard ratingAmount={24} />
+        <UserLandingCard ratingAmount={8} />
+        <UserLandingCard ratingAmount={4} />
+        <UserLandingCard ratingAmount={10} />
+        <UserLandingCard ratingAmount={45} />
+        <UserLandingCard ratingAmount={19} />
       </div>
     </main>
 
--- app/(main)/dashboard/widgets/UserLandingCard.tsx
+++ app/(main)/dashboard/widgets/UserLandingCard.tsx
@@ -1,14 +1,59 @@
 import Image from 'next/image'
+import { Star } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
-export default function UserLandingCard() {
+export default function UserLandingCard({ ratingAmount }: { ratingAmount: number }) {
+
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(0)
+
+  useEffect(() => {
+    console.log('rating', rating)
+  }, [rating])
+
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-row gap-4 p-4 rounded-xl bg-[#180b24]'>
       <div className='flex flex-row gap-4'>
-        <Image src={'/defaultProfilePicture.png'} width={40} height={40} alt='' />
-        <Image src={'/landing-example.jpg'} width={0} height={0} alt='' className='w-20 h-auto object-cover' />
+        <div className='flex flex-col justify-between'>
+          <div className='flex flex-col'>
+            <Image src={'/defaultProfilePicture.png'} width={40} height={40} alt='' className='self-start' />
+            <span className='text-sm self-start'>@John_Doe</span>
+          </div>
+          <Link href={'/dashboard'/* Previsualizar Landing Page con el SDK de StackBlitz */} className='text-sm font-bold py-2 px-4  text-black bg-purple-500 hover:text-purple-500 hover:bg-transparent'>
+            See LandX
+          </Link>
+        </div>
       </div>
-      <div>
-        <span>Rating: 4.5/5</span>
+      <div className='flex flex-col'>
+        <div className='flex flex-row self-center items-center mb-2 gap-1'>
+          <div className='flex flex-row'>
+            {[...Array(5)].map((_, index) => {
+              const currentRating = index + 1
+
+              return (
+                <label key={index} className='flex flex-row'>
+                  <input
+                    type='radio'
+                    name='rating'
+                    value={currentRating}
+                    onClick={() => setRating(currentRating)}
+                    className='relative left-4 hidden'
+                  />
+                  <Star
+                    key={index}
+                    size={20}
+                    onMouseEnter={() => setHover(currentRating)}
+                    onMouseLeave={() => setHover(0)}
+                    className={`star cursor-pointer ${currentRating <= (hover || rating) ? 'fill-yellow-500 stroke-yellow-500' : 'fill-none stroke-black dark:stroke-white'}`}
+                  />
+                </label>
+              )
+            })}
+          </div>
+          <span className='text-sm'>({ratingAmount})</span>
+        </div>
+        <Image src={'/landing-example.jpg'} width={180} height={90} alt='' className='object-cover rounded-xl' />
       </div>
     </div>
   )
